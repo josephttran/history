22c58d96f3f5f0272d10e5eb5de8790438b748dd
josephttran <42635012+josephttran@users.noreply.github.com>
2020-08-25 01:39:56 -0700

    Calculate number of correct match and display correct answer on submit

--- scripts/matching.js
+++ scripts/matching.js
@@ -1,3 +1,5 @@
+let submitted = false;
+
 function getMatchingData() {
     return fetch('/data/match.json')
     .then(response => response.json())
@@ -42,15 +44,56 @@ function displayMatchData(matchingData) {
     for (let i = 0; i < 10; i++)
     {
         const li = document.createElement('li');
+        const p = document.createElement('p');
         const newSelectNode = select.cloneNode(true);
         let randomIndex = Math.floor(Math.random() * statementList.length - 1);
         const randomStatement = statementList.splice(randomIndex, 1);
         const textNode = document.createTextNode(randomStatement);
 
         li.appendChild(newSelectNode);
-        li.appendChild(textNode);
+        p.appendChild(textNode);
+        li.appendChild(p);
         ul.appendChild(li)
     }
 }
 
 getMatchingData().then(data => displayMatchData(data));
+
+const matchSubmitButton = document.querySelector('.match-submit');
+
+matchSubmitButton.addEventListener('click', async function() {
+    if (!submitted) {
+        const data = await getMatchingData();
+        const matchingStatementList = document.querySelectorAll('.matching-statement li');
+        let numberCorrect = 0;
+    
+        matchingStatementList.forEach (li => {
+            const statement = li.querySelector('p');
+            const select = li.querySelector('select');
+            const selectedAnswer = select.options[select.selectedIndex].value;
+    
+            if (data[selectedAnswer] === statement.textContent) {
+                numberCorrect++;
+            }
+            else
+            {
+                const correctAnswer = Object.keys(data).find(key => data[key] === statement.textContent);
+                const correctAnswerP = document.createElement('p');
+                const correctAnswerTextNode = document.createTextNode(`The correct answer is ${correctAnswer}`);
+    
+                correctAnswerP.appendChild(correctAnswerTextNode);
+                correctAnswerP.className = 'correct-answer';
+                li.appendChild(correctAnswerP);
+            }
+        });
+    
+        const main = document.querySelector('main');
+        const resultElement = document.createElement('p');
+        const resultText = document.createTextNode(`Your score is ${numberCorrect}/10`);
+    
+        resultElement.append(resultText);
+        main.appendChild(resultText);
+
+        submitted = true;
+    }
+});
