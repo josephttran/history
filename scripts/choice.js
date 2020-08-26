f30cbea45e3f4f8adebbd393257f9e34ee3c98a4
josephttran <42635012+josephttran@users.noreply.github.com>
2020-08-26 00:14:24 -0700

    Compare user selected choice with correct answer

--- scripts/choice.js
+++ scripts/choice.js
@@ -1,4 +1,5 @@
 let score = 0;
+let isSubmitted = false;
 const numberOfQuestions = 10;
 const multipleChoiceContainer = document.querySelector('.multiple-choice-container');
 const multipleChoiceQuestion = multipleChoiceContainer.querySelector('.multiple-choice-question');
@@ -48,4 +49,39 @@ async function displayQuestionAndPossibleAnswer() {
     }
 }
 
+multipleChoiceSubmitButton.addEventListener('click', async function() {
+    if (!isSubmitted) {
+        let multipleData = await getData();
+        const multipleChoiceQuestion = multipleChoiceContainer.querySelector('.multiple-choice-question');
+        const selectedAnswerNode = multipleChoiceContainer.querySelector('.multiple-choice-possible-answer p.multiple-choice-possible-answer_active');
+
+        if (selectedAnswerNode === null) {
+            alert("Please select an answer");
+        } else {
+            const selectedAnswer = selectedAnswerNode.textContent.slice(3);
+            const obj = multipleData.find(obj => obj.question == multipleChoiceQuestion.textContent);
+
+            multipleChoiceAnswer.textContent = 'The answer is ' + obj.answer;
+            multipleChoiceAnswer.style.display = 'block';
+
+            if (selectedAnswer === obj.answer) {
+                score++;
+                multipleChoiceAnswer.style.color = 'blue';
+            } else {
+                multipleChoiceAnswer.style.color = 'darkred';
+            }
+        }
+        
+        isSubmitted = true;
+    }
+});
+
 displayQuestionAndPossibleAnswer();
+
+// if (location.href.indexOf('/choice') !== -1) {
+//     window.addEventListener('beforeunload', function(e) {
+//         var confirmationMessage = "\o/";
+//         (e || window.event).returnValue = confirmationMessage;
+//         return confirmationMessage;
+//     });
+// }
